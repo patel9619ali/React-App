a81a0afeb601a804c11aa7300a9931aa67832451
Patel Mohd Ali <patel9619ali@gmail.com>
2022-12-16 01:42:37 +0530

    Use State

--- src/Component/Expence Component/ExpenceItems.js
+++ src/Component/Expence Component/ExpenceItems.js
@@ -1,19 +1,23 @@
+import React , { useState } from 'react'
 import ExpenceHeading from '../Expence Component/ExpenceHeading';
 import ExpenceAmount from '../Expence Component/ExpenceAmount';
 import ExpenseDate from '../Expence Component/ExpenseDate';
 import Card from '../Expence Component/Card';
 import '../Expence Component/ExpenceWrapper.css'
 import '../Expence Component/ExpenceHeading.css'
+import '../Expence Component/Button.css'
 const ExpenceItem = props => {
-    const clickHandler = () => {
-        console.log('Clicked')
+    let [title,newTitle] = useState(props.title);
+    const clickHandler = ()=>{
+        newTitle('Updated');
+        console.log(title)
     }
     return(
         <Card className="expence_wrapper">
             <ExpenseDate dates={props.dates} />
-            <ExpenceHeading heading = {props.title}/>
+            <ExpenceHeading heading = {title}/>
             <ExpenceAmount amounts={props.amount}/>
-            <button onClick={clickHandler} className="btn_click">Click Here</button>
+            <button className={"click_button"} onClick={clickHandler}>Click Here to Update</button>
         </Card>
     );
 };
